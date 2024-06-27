b84676723a5a87bd3c28a9037c97fe2aa2d5164e
babu j <babu.j@techouts.com>
2024-06-28 02:06:17 +0530

    The tricky questions have been completed.

---
+++ src/com/tech/tricky/PrintNumbersWithoutLoop.java
@@ -0,0 +1,15 @@
+package com.tech.tricky;
+
+/*Print 1 to 100 numbers without using loops*/
+
+public class PrintNumbersWithoutLoop {
+	public static void printNumbers(int number) {
+		if(number<=100) {
+			System.out.println(number);
+			printNumbers(number+1);
+		}
+	}
+	public static void main(String[] args) {
+		printNumbers(1);
+	}
+}
