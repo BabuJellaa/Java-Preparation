1045faafa1f6896513eae000408193643ee2fe0d
babu j <babu.j@techouts.com>
2024-08-08 13:58:38 +0530

    added oldest and youngest employees Code

--- src/com/tech/collections/TechoutsEmployee.java
+++ src/com/tech/collections/TechoutsEmployee.java
@@ -9,11 +9,11 @@ import java.util.Map;
 public class TechoutsEmployee {	
 	public static void main(String[] args) {
 		List<TechoutsEmployee> employees=new ArrayList<TechoutsEmployee>();
-		employees.add(new TechoutsEmployee("Babu Jella", 30000, 23, "Techouts","Male",LocalDate.of(2022, 2, 14),"HR"));
-		employees.add(new TechoutsEmployee("Venkatesh Karamkanti", 31000, 23, "TCS","Male",LocalDate.of(2023, 3, 15),"HR"));
-		employees.add(new TechoutsEmployee("Ajay Durgam", 32000, 23, "Capgemini","Male",LocalDate.of(2024, 4, 16),"IT"));
-		employees.add(new TechoutsEmployee("Paddu Reddy", 32000, 23, "Accenture","FeMale",LocalDate.of(2025, 5, 17),"Finance"));
-		employees.add(new TechoutsEmployee("Dhanusha Jella", 32000, 23, "Peddalingapur","FeMale",LocalDate.of(2026, 6, 18),"Finance"));
+		employees.add(new TechoutsEmployee("Babu Jella", 30000, 21, "Techouts","Male",LocalDate.of(2022, 2, 14),"HR"));
+		employees.add(new TechoutsEmployee("Venkatesh Karamkanti", 31000, 22, "TCS","Male",LocalDate.of(2023, 3, 15),"HR"));
+		employees.add(new TechoutsEmployee("Ajay Durgam", 32000, 24, "Capgemini","Male",LocalDate.of(2024, 4, 16),"IT"));
+		employees.add(new TechoutsEmployee("Paddu Reddy", 32000, 21, "Accenture","FeMale",LocalDate.of(2025, 5, 17),"Finance"));
+		employees.add(new TechoutsEmployee("Dhanusha Jella", 32000, 25, "Peddalingapur","FeMale",LocalDate.of(2026, 6, 18),"Finance"));
 
 		/*Find the Avarage salary of the Male and Female employees in the organization*/
 		employeesSalaryAvarage(employees);
@@ -24,6 +24,12 @@ public class TechoutsEmployee {
 
 		/* Increment the salary of employeesin the HR department by 10 percent */
 		hrSalaryIncrement(employees,10);
+		
+		/* Find the oldest employee in the Organization */
+		getOldestEmployee(employees);
+		
+		/* Find the youngest employee in the Organization */
+		getYoungestEmployee(employees);
 	}
 
 	private static void employeesSalaryAvarage(List<TechoutsEmployee> employees) {
@@ -45,6 +51,7 @@ public class TechoutsEmployee {
 		int maleAvarageSalary=maleSalarySum/maleCount;
 
 		System.out.println("Avarage of Male Salary : "+maleAvarageSalary+"Avarage of Female Salary : "+ femaleAvarageSalary);
+		System.out.println();
 	}
 
 	private static void employeeJoinedSpecificYear(List<TechoutsEmployee> employees,int joiningYear) {
@@ -54,8 +61,10 @@ public class TechoutsEmployee {
 				techoutsEmployee.add(employee);
 			}
 		}
+		System.out.println("Employee who joined in the Specific year : ");
 		for(TechoutsEmployee employee : techoutsEmployee) {
 			System.out.println("Employee name : "+employee.name+" with year of "+ employee.dateOfJoining);
+			System.out.println();
 		}
 	}
 
@@ -65,9 +74,45 @@ public class TechoutsEmployee {
 				employee.setSalary((long) ((employee.getSalary())+((employee.getSalary())*(hikePercentage/100))));
 			}
 		}
+		System.out.println("HR salary increment : ");
 		for(TechoutsEmployee employee : employees) {
 			System.out.println(employee);
 		}
+		System.out.println();
+	}
+	
+	private static void getOldestEmployee(List<TechoutsEmployee> employees) {
+		int age=0;
+		List<TechoutsEmployee> oldestEmployee = new ArrayList<TechoutsEmployee>();
+		for(TechoutsEmployee employee : employees) {
+			if(employee.getAge()>=age) {
+				age=employee.getAge();
+			}
+		}
+		for(TechoutsEmployee employee : employees) {
+			if(employee.getAge() == age) {
+				oldestEmployee.add(employee);
+			}
+		}
+		System.out.println("Oldest Employee : "+oldestEmployee);
+		System.out.println();
+	}
+	
+	private static void getYoungestEmployee(List<TechoutsEmployee> employees) {
+		List<TechoutsEmployee> youngestEmployee = new ArrayList<TechoutsEmployee>();
+		int age=employees.get(0).age;
+		for(TechoutsEmployee employee : employees) {
+			if(employee.getAge()<=age) {
+				age=employee.getAge();
+			}
+		}
+		for(TechoutsEmployee employee : employees) {
+			if(employee.getAge() == age) {
+				youngestEmployee.add(employee);
+			}
+		}
+		System.out.println("Youngest Employee : "+youngestEmployee);
+		System.out.println();
 	}
 
 	private String name;
@@ -131,7 +176,7 @@ public class TechoutsEmployee {
 
 	@Override
 	public String toString() {
-		return "TechoutsEmployee [name=" + name + ", salary=" + salary + ", department=" + department + "]";
+		return "TechoutsEmployee [name=" + name + ", salary=" + salary + ", department=" + department +",  with age =" + age + "]";
 	}
 
 }
